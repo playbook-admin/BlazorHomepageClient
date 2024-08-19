43b41e565f8555e7aedb6e4841f19c7abd9cc92d
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-08-19 18:30:32 +0200

    Some corrections

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,8 +10,8 @@
 
 window.getDynamicStyle = (spriteType) => {
   const dayNumber = new Date().getDay() % 12; // Get day of the week and limit to 12
-  const offset = -82 * dayNumber; // Calculate the offset
-  return `background: transparent url('/images/Bootstrap${spriteType}Composed.jpg') 0 ${offset}px; height: 81px; margin: 0;`;
+  const offset = -81 * dayNumber; // Calculate the offset
+  return `background: url('/images/Bootstrap${spriteType}Composed.jpg') 0 ${offset}px transparent; height: 81px; margin: 0;`;
 };
 
 

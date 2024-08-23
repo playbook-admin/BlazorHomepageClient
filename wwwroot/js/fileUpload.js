b2592cdc2d6602e40f4b4e295c09c892a53a1e4b
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-08-23 18:08:46 +0200

    Added fileUpload.js

---
+++ wwwroot/js/fileUpload.js
@@ -0,0 +1,26 @@
+﻿window.fileUpload = {
+  initializeDropZone: function (dotNetHelper) {
+    const dropZone = document.getElementById("dropZone");
+
+    dropZone.addEventListener('dragenter', function (e) {
+      e.preventDefault();
+      dotNetHelper.invokeMethodAsync('HandleDragEnter');
+    });
+
+    dropZone.addEventListener('dragover', function (e) {
+      e.preventDefault();
+      dotNetHelper.invokeMethodAsync('HandleDragOver');
+    });
+
+    dropZone.addEventListener('dragleave', function (e) {
+      e.preventDefault();
+      dotNetHelper.invokeMethodAsync('HandleDragLeave');
+    });
+
+    dropZone.addEventListener('drop', function (e) {
+      e.preventDefault();
+      const files = e.dataTransfer.files;
+      dotNetHelper.invokeMethodAsync('HandleDrop', Array.from(files).map(f => f.name));
+    });
+  }
+};
